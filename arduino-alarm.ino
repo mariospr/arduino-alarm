874c43ae523911609d2becac87a11486ddb9ec8c
Mario Sanchez Prada <mario@mariospr.org>
2014-11-09 12:45:17 +0000

    Add GPLv3 license details

--- arduino-alarm.ino
+++ arduino-alarm.ino
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2014 Mario Sanchez Prada
+ * Authors: Mario Sanchez Prada <mario@mariospr.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 3 of the GNU General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ */
 #define TOGGLE_SWITCH_PIN 13
 #define LIGHT_SENSOR_PIN A0
 
